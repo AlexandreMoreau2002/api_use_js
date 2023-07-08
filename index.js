2c71643ee67522f1f1ed17bea34612b19538de54
AlexGrimpeur <moreaualexandre2002@gmail.com>
2023-07-08 16:59:16 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+const button = document.querySelector(".btn");
+const img = document.querySelector(".photo");
+
+async function imgAtPageChargement() {
+    try {
+        const response = await fetch('https://cataas.com/cat');
+        const data = await response.url;
+        img.src = data;
+    } catch (error) {
+        console.log(error);
+    }
+} imgAtPageChargement();
+
+    console.log("Bonjour, monde");
+
+    // Appel de la fonction pour charger l'image
+
+    // Appel avec fetch :
+    button.addEventListener('click', async () => {
+        try {
+            const fetchResult = await fetch('https://cataas.com/cat');
+            const data = await fetchResult.url;
+
+            console.log(data);
+            img.src = data;
+        } catch (error) {
+            console.log(error);
+        }
+    });
+});
